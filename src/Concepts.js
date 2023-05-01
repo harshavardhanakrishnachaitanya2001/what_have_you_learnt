05c494c0d5aa30c702fa7bbc086b71be8ee9d3ce
G. H. Krishna Chaitanya <g.h.krishnachaitanya@Gs-Mac-mini.local>
2023-05-01 10:01:55 +0530

    All that is left fot this app, is styling it

---
+++ src/Concepts.js
@@ -0,0 +1,37 @@
+export const concepts = [
+    {
+      id: 1,
+      name: "HTML",
+      learned: false,
+    },
+    {
+      id: 2,
+      name: "CSS",
+      learned: false,
+    },
+    {
+      id: 3,
+      name: "JavaScript",
+      learned: false,
+    },
+    {
+      id: 4,
+      name: "Reactjs",
+      learned: false,
+    },
+    {
+      id: 5,
+      name: "Tailwindcss",
+      learned: false,
+    },
+    {
+      id: 6,
+      name: "Mongodb",
+      learned: false,
+    },
+    {
+      id: 7,
+      name: "Expressjs",
+      learned: false,
+    },
+  ]
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,60 +1,85 @@
-import React from 'react'
-import { useState } from 'react'
-const Form = () => {
-  const [name,setName] = useState('');
-  const [age,setAge] = useState(0)
-  const [details,setDetails] = useState({})
-  const [concepts,setConcepts] = useState([
-    {
-      id:1,
-      name:'HTML',
-      learned:false
-    },
-    {
-      id:2,
-      name:'CSS',
-      learned:false
-    },
-    {
-      id:3,
-      name:'JavaScript',
-      learned:false
-    },
-    {
-      id:4,
-      name:'Reactjs',
-      learned:false
-    },
-    {
-      id:5,
-      name:'Tailwindcss',
-      learned:false
-    },
-    {
-      id:6,
-      name:'Mongodb',
-      learned:false
-    },
-    {
-      id:7,
-      name:'Expressjs',
-      learned:false
-    },
-  ])
-  return (
-    <form>
-      {/* Name field */}
-      <div>
-        <label>Name</label>
-        <input type='text' value = {name}/>
-      </div>
+import React from "react";
+import { useState } from "react";
+import {concepts} from '../Concepts'
 
-      {/* Age field */}
-      <div>
-        <label>Age</label>
-        <input type='text' />
-      </div>
-    </form>
+ const Form = () => {
+  const [name, setName] = useState("");
+  const [details, setDetails] = useState({
+    learnedConcepts: [], 
+    yetToLearn: []
+  });
+
+  const handleName = (e) => {
+    e.preventDefault();
+    setName(e.target.value);
+  };
+  const handleCheckbox = (e) => {
+    const checkedCheckbox = e.target.name;
+    const isChecked = e.target.checked;
+    const concept = concepts.find((c) => c.name === checkedCheckbox);
+    concept.learned = isChecked;
+
+
+    let learnedConcepts = concepts.filter(concept => concept.learned===true);
+    let conceptsYetToLearn = concepts.filter(concept => concept.learned!==true);
+    setDetails({...details,learnedConcepts:learnedConcepts,yetToLearn:conceptsYetToLearn})
+    
+  }
+
+  // Storing name details object
+  const saveDetails = (e) => {
+    e.preventDefault();
+    setDetails(prevDetails => ({ ...prevDetails, personName: name }));
+  };
+  
+  return (
+    <>
+      <form>
+        {/* Name field */}
+        <div>
+          <label>Name</label>
+          <input
+            type="text"
+            value={name}
+            onChange={handleName}
+            name="personName"
+            required
+          />
+        </div>
+
+        {/* List of concepts */}
+        <ul>
+          {concepts.map((concept) => (
+            <li key={concept.id}>
+              <input
+                checked={concepts.learned}
+                type="checkbox"
+                onChange={handleCheckbox}
+                name={concept.name}
+              />
+              {concept.name}
+            </li>
+          ))}
+        </ul>
+
+      </form>
+        {/* Result display area */}
+        <button onClick={saveDetails}>Save</button>
+        <span>{details.personName}, You know: </span>
+        <ol>
+          {
+            details.learnedConcepts.map(concept => (
+              <li key={concept.id}>{concept.name}</li>
+            ))
+          }
+        </ol>
+        <h1>and you need to learn</h1>
+        {
+          details.yetToLearn.map(concept => (
+            <li key={concept.id}>{concept.name}</li>
+          ))
+        }
+    </>
   )
 }
 
